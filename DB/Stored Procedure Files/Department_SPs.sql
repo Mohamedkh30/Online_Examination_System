b11566aa0339a940a4013af42b07bd83f6ba84d2
Mohamedkh30 <70284819+Mohamedkh30@users.noreply.github.com>
2023-03-20 18:24:31 +0200

    Add files via upload

--- DB/Stored Procedure Files/Department_SPs.sql
+++ DB/Stored Procedure Files/Department_SPs.sql
@@ -1,27 +1,39 @@
 create proc InsertDepartment  @name varchar(50), @MngrId int
 as
-	declare @MId int
-	select @MId=Ins_Id from Instructor where @MngrId=Ins_Id
+if not exists (select * from Department where Dept_Name=@name)
+	begin
+		declare @MId int
+		select @MId=Ins_Id from Instructor where @MngrId=Ins_Id
 
-	if(@MId is not null)
-		begin
-			insert into Department values(@name,@MngrId)
-		end
-	else
-		select 'Invalid Manager ID'
-
--- InsertDepartment  555,10
+		if(@MId is not null)
+	
+			begin
+				insert into Department values(@name,@MngrId)
+			end
+		else
+			select 'Invalid Manager ID'
+	end
+else
+	select 'the department already exists'
+	go
+-- InsertDepartment  555,3
 -----------------------------------------------------------------------------------------------------------------------
-create proc DeleteDepartmentById @id int				--modify it to delete all students,courses,.... within
+create proc DeleteDepartmentById @id int
 as
-delete from Department where Dept_Id=@id
-
--- DeleteDepartmentById 101
+if exists (select * from Department where Dept_Id=@id)
+	delete from Department where Dept_Id=@id
+else
+	select 'Invalid department ID'
+go
+-- DeleteDepartmentById 4
 -----------------------------------------------------------------------------------------------------------------------
 create proc DeleteDepartmentByName @name varchar(50)			
 as
-delete from Department where Dept_Name=@name
-
+if exists (select * from Department where Dept_Name=@name)
+	delete from Department where Dept_Name=@name
+else
+	select 'Invalid department Name'
+go
 -- DeleteDepartmentByName 555
 -----------------------------------------------------------------------------------------------------------------------
 create proc SelectDepartments 
@@ -32,27 +44,63 @@ select * from Department
 -----------------------------------------------------------------------------------------------------------------------
 create proc SelectDepartmentById @id int
 as
-select * from Department where Dept_Id=@id
+if exists (select * from Department where Dept_Id=@id)
+	select * from Department where Dept_Id=@id
+else
+	select 'Invalid department ID'
+go
 
--- SelectDepartmentById 2
+-- SelectDepartmentById 9
 -----------------------------------------------------------------------------------------------------------------------
 create proc SelectDepartmentByName @name varchar(50)
 as
-select * from Department where Dept_Name=@name
+if exists (select * from Department where Dept_Name=@name)
+	select * from Department where Dept_Name=@name
+else
+	select 'Invalid department Name'
+go
 
 -- SelectDepartmentByName 'Professional Web Development'
 -----------------------------------------------------------------------------------------------------------------------
 create proc UpdateDepartmentById @id int, @name varchar(50), @MngrId int
 as
-	declare @MId int
-	select @MId=Ins_Id from Instructor where @MngrId=Ins_Id
+if exists (select * from Department where Dept_Id=@id)
+	begin
+		declare @MId int
+		select @MId=Ins_Id from Instructor where @MngrId=Ins_Id
 
-	if(@MId is not null)
-		begin
-			update Department set Dept_Name=@name,Manager_Id=@MngrId where Dept_Id=@id
-		end
-	else
-		select 'Invalid Manager ID'
+		if(@MId is not null)
+			begin
+				update Department set Dept_Name=@name,Manager_Id=@MngrId where Dept_Id=@id
+			end
+		else
+			select 'Invalid Manager ID'
+	end
+else
+	select 'Invalid department ID'
+go
+	
 
--- UpdateDepartmentById 101,666,101
+-- UpdateDepartmentById 6,666,5
+-----------------------------------------------------------------------------------------------------------------------
+create proc UpdateDepartmentByName @name varchar(50), @MngrId int
+as
+if exists (select * from Department where Dept_Name=@name)
+	begin
+		declare @MId int
+		select @MId=Ins_Id from Instructor where @MngrId=Ins_Id
+
+		if(@MId is not null)
+			begin
+				update Department set Manager_Id=@MngrId where Dept_Name=@name
+			end
+		else
+			select 'Invalid Manager ID'
+	end
+else
+	select 'Invalid department Name'
+go
+	
+
+-- UpdateDepartmentById 6,666,5
 -----------------------------------------------------------------------------------------------------------------------
