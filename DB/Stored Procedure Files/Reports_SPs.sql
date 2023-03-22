1ad585ac55d06bc9b1f37bdbd6e4c01fb239e04a
Mohamedkh30 <70284819+Mohamedkh30@users.noreply.github.com>
2023-03-22 23:58:17 +0200

    Add files via upload

--- DB/Stored Procedure Files/Reports_SPs.sql
+++ DB/Stored Procedure Files/Reports_SPs.sql
@@ -66,11 +66,71 @@ GO
 
 --	Report that takes exam number and returns the Questions in it and chocies [freeform report]
 
+CREATE Proc [dbo].[GetExamQuestionsAndChoices] @Exam_Num int
+AS
+	DECLARE @QID int, @QContent varchar(1000),@QType varchar(10) ,@choice1 varchar(150),@choice2 varchar(150),@choice3 varchar(150),@choice4 varchar(150)
+	DECLARE Question_cursor CURSOR FOR
+		select q.Q_Id,Q_Content,[type] from Exam_Questions as eq , Question as q
+		where
+		eq.Q_Id = q.Q_Id and
+		Exam_Num = @Exam_Num
+
+	OPEN Question_cursor
+	FETCH Question_cursor INTO @QID,@QContent,@QType
+
+	while @@FETCH_STATUS=0
+		begin
+		
+			if (@QType='MCQ')
+				begin
+					
+					DECLARE choice_cursor CURSOR FOR
+					select Choice_Content from Choice where Q_Id=@QID
+
+					declare @choice varchar(150)
+					open choice_cursor
+					
+					FETCH NEXT FROM choice_cursor INTO @choice
+
+					IF @@FETCH_STATUS = 0
+						SET @choice1 = @choice;
+
+					FETCH NEXT FROM choice_cursor INTO @choice
+
+					IF @@FETCH_STATUS = 0
+						SET @choice2 = @choice;
+
+					FETCH NEXT FROM choice_cursor INTO @choice
+
+					IF @@FETCH_STATUS = 0
+						SET @choice3 = @choice;
+
+					FETCH NEXT FROM choice_cursor INTO @choice
+
+					IF @@FETCH_STATUS = 0
+						SET @choice4 = @choice;
+
+					close choice_cursor
+					deallocate choice_cursor
+
+					select @QContent,@choice1,@choice2,@choice3,@choice4
+				end
+
+			else if(@QType='TF')
+				begin
+					set @choice1='True'
+					set @choice2='False'
+
+					select @QContent,@choice1,@choice2
+				end
+
+			FETCH Question_cursor INTO @QID,@QContent,@QType
+		end
+
+	close Question_cursor
+	deallocate Question_cursor
 
 --	Report that takes exam number and the student ID then returns the Questions in this exam with the student answers
-
-	--------------------------------------------
---stored of last report 
 
  create proc Select_Student_Answer @Student_Id varchar(4) , @Exam_Num varchar(3)
  as 
